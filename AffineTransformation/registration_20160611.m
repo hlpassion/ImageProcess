4eaf4376b0339dec7fde68ef14a9ac9b877e1bff
Hailon <dyzhl15@126.com>
2016-06-12 18:27:35 +0800

    add an test picture
    fix_test.tiff
    add one moving image

--- AffineTransformation/registration_20160611.m
+++ AffineTransformation/registration_20160611.m
@@ -1,6 +1,9 @@
 fixed = rgb2gray(imread('fix.tiff'));
 % moving = imrotate(fixed, 5, 'bilinear', 'crop');
+
 moving = rgb2gray(imread('05.tiff'));
+%test the other option
+% moving = rgb2gray(imread('fix_test.tiff'));
 imshowpair(fixed, moving,'montage');
 [optimizer, metric] = imregconfig('multimodal');
 tform = imregtform(moving, fixed, 'similarity', optimizer, metric);%Translation, %, Scale
